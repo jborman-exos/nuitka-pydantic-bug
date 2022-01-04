783b0a465fc1c0c80a6b7d406b62db76f5bc7f8a
jborman-exos <johncraig.borman@exosfinancial.com>
2022-01-04 15:51:56 -0500

    Cleaned up test case

--- test_my_module.py
+++ test_my_module.py
@@ -3,14 +3,16 @@ import my_module as mm
 def test_minimum_failure() -> None:
     a = mm.A()
     b = mm.B(member_a=a)
-
+    print(f'b: {b}', type(b))
+    print(f'b.member_a: {b.member_a}', type(b.member_a))
     b.bar(bar_arg='some argument')
 
 def test_working() -> None:
     a = mm.A()
-    b = mm.B2(member_a=a)
-
-    b.bar(bar_arg='some argument')
+    b2 = mm.B2(member_a=a)
+    print(f'b2: {b2}', type(b2))
+    print(f'b2.member_a: {b2.member_a}', type(b2.member_a))
+    b2.bar(bar_arg='some argument')
 
 if __name__ == '__main__':
     test_working()
